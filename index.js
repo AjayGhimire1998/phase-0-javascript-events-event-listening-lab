b5c7b66261c83a465fa09f8e6321d37fe41b9c7c
AjayGhimire <ajayghimire1998@gmail.com>
2022-03-05 16:32:49 +1100

    done with the lab

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+// const input = document.querySelector('input');
+// function clickAlert() {
+//     alert ('I was clicked !!!!');
+// }
+// input.addEventListener('click', clickAlert);
+
+// const input = document.querySelector('input');
+// input.addEventListener('click', function(){
+//     alert ('I was clicked!!');
+// });
+
+// const input = document.querySelector('input');
+
+// const addingEventListener = input.addEventListener('click', function(){
+//     alert ('i was clicked!');
+// });
+// console.log(addingEventListener());
+
 function addingEventListener() {
+    const input = document.getElementById('input');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
